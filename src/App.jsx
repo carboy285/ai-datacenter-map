adfad94aacca6d3c131eeb1557cf7833dee2bfa6
carboy285 <bsimpson2850@gmail.com>
2026-06-13 21:50:16 -0500

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{fontFamily:'Arial',padding:'40px',textAlign:'center'}}>
-      <h1>AI Data Center Atlas</h1>
-      <p>Deployment successful.</p>
-    </div>
-  );
-}
